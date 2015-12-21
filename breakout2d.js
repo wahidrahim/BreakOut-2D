470ffa0b8712d0a959499104fa5af33848489e69
Wahid Rahim <wahidrahim@gmail.com>
2015-12-21 12:24:45 -0500

    moving paddle

--- breakout2d.js
+++ breakout2d.js
@@ -8,10 +8,46 @@ var dy = -2;
 
 var ball_radius = 10;
 
+var paddle_height = 15;
+var paddle_width = 100;
+var paddle_x = (canvas.width - paddle_width) / 2;
+
+var right_pressed = false;
+var left_pressed = false;
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    right_pressed = true;
+  }
+  else if (e.keyCode == 37) {
+    left_pressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    right_pressed = false;
+  }
+  else if (e.keyCode == 37) {
+    left_pressed = false;
+  }
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle_x, canvas.height - paddle_height - 2, paddle_width, paddle_height);
+  ctx.fillStyle = '#9933ff';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ball_radius, 0, Math.PI*2);
-  ctx.fillStyle = "#FF0090";
+  ctx.fillStyle = '#ff00ff';
   ctx.fill();
   ctx.closePath();
 }
@@ -20,6 +56,7 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawBall();
+  drawPaddle();
 
   if (x + dx < ball_radius || x + dx > canvas.width - ball_radius) {
     dx = -dx;
@@ -28,8 +65,16 @@ function draw() {
     dy = -dy;
   }
 
+  if (right_pressed && paddle_x < canvas.width - paddle_width) {
+    paddle_x += 5;
+  }
+  else if (left_pressed && paddle_x > 0) {
+    paddle_x -= 5;
+  }
+
   x += dx;
   y += dy;
 }
+
 
 setInterval(draw, 5);
