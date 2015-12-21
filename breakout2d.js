cf0b88e1915479990458cfd5957792bcb8574b91
Wahid Rahim <wahidrahim@gmail.com>
2015-12-21 11:57:55 -0500

    moving ball

---
+++ breakout2d.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 20, 0, Math.PI*2);
+  ctx.fillStyle = "#FF0090";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawBall();
+
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 5);
