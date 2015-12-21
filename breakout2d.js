14ee0725d2b35acbe73ba3ccd2cd82fadd35f83d
Wahid Rahim <wahidrahim@gmail.com>
2015-12-21 12:04:32 -0500

    bouncing off walls

--- breakout2d.js
+++ breakout2d.js
@@ -6,9 +6,11 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
+var ball_radius = 10;
+
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 20, 0, Math.PI*2);
+  ctx.arc(x, y, ball_radius, 0, Math.PI*2);
   ctx.fillStyle = "#FF0090";
   ctx.fill();
   ctx.closePath();
@@ -18,6 +20,13 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawBall();
+
+  if (x + dx < ball_radius || x + dx > canvas.width - ball_radius) {
+    dx = -dx;
+  }
+  if (y + dy < ball_radius || y + dy > canvas.height - ball_radius) {
+    dy = -dy;
+  }
 
   x += dx;
   y += dy;
